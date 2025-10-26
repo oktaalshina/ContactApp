94f5734008cb412f50930c16259b142beee36ba1
oktaalshina <oktaalshinaarvaparahyta@mail.ugm.ac.id>
2025-10-26 19:10:40 +0700

    Membuat actionbar untuk navigasi ke halaman profile

--- app/src/main/java/com/example/contactapp/MainActivity.kt
+++ app/src/main/java/com/example/contactapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.contactapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.contactapp.databinding.ActivityMainBinding
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         checkLoginStatus()
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
 
         // Handle UI dengan binding
         with(binding) {
@@ -51,5 +55,25 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // "Inflate" atau pasang menu yang sudah kamu buat
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Cek item mana yang diklik berdasarkan ID-nya
+        when (item.itemId) {
+            R.id.action_profile -> {
+                // Kode untuk pindah ke ProfileActivity
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+
+                return true // Kembalikan true karena kita sudah menangani klik-nya
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
